d09097b79298068c9105fa29b9086599a8aca406
Aurélien Baronnet <aykarian@gmail.com>
2019-10-28 22:46:32 +0100

    Add files via upload

---
+++ Assignment4/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment4;
+using Microsoft.VisualBasic;
+
+namespace DatabaseService
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Required { get; set; }
+        public DateTime Shipped { get; set; }
+        public int Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipCity { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ Assignment4/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DatabaseService;
+
+namespace Assignment4
+{
+    public class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int Discount { get; set; }
+        public Product Product { get; set; }
+        public Order Order { get; set; }
+    }
+}
